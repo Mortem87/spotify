5fbfcece5389835e4c417472c6218b9914f8d371
Pablo Juarez <pablojuarez87@gmail.com>
2018-07-05 11:01:01 -0700

    login devuelve usuario.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 import { User } from './models/user';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [UserService]
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
   public title = 'Musify';
   public user: User;
   public identity;
   public token;
 
-  constructor(){
+  constructor(
+  	private _userService:UserService
+  	){
   	this.user = new User('', '', '', '', '', 'ROL_USER', '');
   }
+  ngOnInit(){
+  	//var texto = this._userService.signup(this.user);
+  	//console.log(texto);
+  }
+
   public onSubmit(){
   	console.log(this.user);
+
+  	this._userService.signup(this.user).subscribe(
+  		response => {
+  			console.log(response);
+  		},
+  		error => {
+  			var errorMessage = <any>error;
+
+  			if(errorMessage != null){
+  				console.log(error);
+  			}
+  		}
+  	);
   }
 }
